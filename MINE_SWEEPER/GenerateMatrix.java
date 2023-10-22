75ba7b8cd80c3e1eecfa44377ffbd0950a1e6b48
Saicharan-Yadav <saicharanyadav.p20@iiits.in>
2023-10-22 12:07:00 +0530

    matrix Generator initiated

---
+++ MINE_SWEEPER/GenerateMatrix.java
@@ -0,0 +1,24 @@
+package MINE_SWEEPER;
+
+import java.util.*;
+
+public class GenerateMatrix {
+    static class pair {
+        int row, col;
+
+        pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+
+        Random rand = new Random();
+
+        int arr[][] = new int[n][n];
+        int mines = 20;
+
+    }
+}
