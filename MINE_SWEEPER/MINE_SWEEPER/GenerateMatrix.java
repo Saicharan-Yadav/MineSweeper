c6a0c212e39171017517b603e98c0d15fe6e118b
Saicharan-Yadav <saicharanyadav.p20@iiits.in>
2024-02-05 10:56:25 +0530

    updated with difficulty level, error messages

--- MINE_SWEEPER/MINE_SWEEPER/GenerateMatrix.java
+++ MINE_SWEEPER/MINE_SWEEPER/GenerateMatrix.java
@@ -1,23 +1,25 @@
+// package mines;
 // package MINE_SWEEPER;
 
 import java.util.*;
-
-import javax.print.DocFlavor.STRING;
 
 class GenerateMatrix {
     int n;
     int mines;
 
+    // constructor
     GenerateMatrix(int n, int mines) {
         this.n = n;
         this.mines = mines;
     }
 
+    // done
     public boolean[][] genShowMatrix(int n) {
         boolean arr[][] = new boolean[n][n];
         return arr;
     }
 
+    // done
     public int[][] genMatrix(int n1, int mines1) {
         int n = n1;
         int mines = mines1;
@@ -28,8 +30,8 @@ class GenerateMatrix {
         Random rand = new Random();
 
         while (s.size() < mines) {
-            int x = rand.nextInt(9);
-            int y = rand.nextInt(9);
+            int x = rand.nextInt(n);
+            int y = rand.nextInt(n);
             String s1 = x + "," + y;
             s.add(s1);
         }
@@ -44,6 +46,7 @@ class GenerateMatrix {
         return arr;
     }
 
+    // done
     public void IncreaseCountOfSurroundings(int[][] arr, int i, int j) {
         int n = arr.length;
 
@@ -62,6 +65,7 @@ class GenerateMatrix {
         }
     }
 
+    // done
     public void validate(int n, int mines, int arr[][]) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -72,6 +76,7 @@ class GenerateMatrix {
         }
     }
 
+    // done
     public void printall(int[][] arr, boolean[][] disparr, Set<String> pairArr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
@@ -84,6 +89,7 @@ class GenerateMatrix {
         printArray(arr, disparr, pairArr);
     }
 
+    // Revisit for styling
     public void printArray(int[][] arr, boolean[][] disparr, Set<String> pairArr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -93,12 +99,14 @@ class GenerateMatrix {
         }
     }
 
+    // Done
     public void printArray(int[][] arr, boolean[][] disparr, Set<String> pairArr, int row, int col) {
+        System.out.println(pairArr);
         Stack<String> st = new Stack<>();
         Stack<String> rep = new Stack<>();
         st.add(row + "," + col);
         if (arr[row][col] == 0) {
-            System.out.println("0 encountered");
+
             while (!st.isEmpty()) {
                 String curr = st.pop();
                 rep.add(curr);
@@ -119,7 +127,6 @@ class GenerateMatrix {
                                 int dcol = j + cols[j1];
                                 String dummy = drow + "," + dcol;
 
-                                System.out.println("dummy called" + dummy);
                                 if (arr[drow][dcol] == 0) {
                                     if (!rep.contains(dummy)) {
                                         System.out.println("true");
@@ -152,86 +159,5 @@ class GenerateMatrix {
         }
 
     }
-    /**
-     * public void printArray(int[][] arr, boolean[][] disparr, Set<String> pairArr,
-     * boolean flag, int row, int col) {
-     * Stack<String> st = new Stack<>();
-     * st.push(row + "," + col);
-     * if (arr[row][col] == 0) {
-     * while (!st.isEmpty()) {
-     * String stop = st.pop();
-     * String sarr[] = stop.split(",");
-     * // System.out.println(sarr);
-     * System.out.println("**************************************");
-     * for (int i = 0; i < arr.length; i++) {
-     * for (int j = 0; j < arr[i].length; j++) {
-     * System.out.print(arr[i][j] + " ");
-     * }
-     * System.out.println(); // Move to the next line after each row
-     * }
-     * System.out.println("**************************************");
-     * 
-     * int srow = Integer.parseInt(sarr[0]);
-     * int scol = Integer.parseInt(sarr[1]);
-     * 
-     * int rows[] = { -1, 0, 1 };
-     * int cols[] = { -1, 0, 1 };
-     * for (int i1 = 0; i1 < 3; i1++) {
-     * for (int j1 = 0; j1 < 3; j1++) {
-     * if (i1 == 1 && j1 == 1)
-     * continue;
-     * else {
-     * if (srow + rows[i1] >= 0 && srow + rows[i1] < n && scol + cols[j1] >= 0
-     * && scol + cols[j1] < n) {
-     * String dummy = srow + "" + rows[i1] + "," + scol + "" + cols[j1];
-     * if (arr[srow + rows[i1]][scol + cols[j1]] == 9)
-     * continue;
-     * if (arr[srow + rows[i1]][scol + cols[j1]] == 0) {
-     * st.push(dummy);
-     * }
-     * pairArr.add(dummy);
-     * disparr[srow + rows[i1]][scol + cols[j1]] = true;
-     * }
-     * }
-     * }
-     * }
-     * 
-     * }
-     * }
-     * if (flag == true) {
-     * for (int i = 0; i < arr.length; i++) {
-     * for (int j = 0; j < arr.length; j++) {
-     * if (arr[i][j] == 9)
-     * pairArr.add(i + "," + j);
-     * disparr[i][j] = true;
-     * }
-     * }
-     * }
-     * for (String s : pairArr) {
-     * String sarr[] = s.split(",");
-     * int i = Integer.parseInt(sarr[0]);
-     * int j = Integer.parseInt(sarr[1]);
-     * disparr[i][j] = true;
-     * }
-     * for (int i = 0; i < arr.length; i++) {
-     * for (int j = 0; j < arr[i].length; j++) {
-     * System.out.print(disparr[i][j] ? arr[i][j] + " " : "* ");
-     * }
-     * System.out.println();
-     * }
-     * 
-     * }
-     */
 
-    /**
-     * public static void main(String[] args) {
-     * int n = 9;
-     * int mines = 20;
-     * 
-     * int arr[][] = genMatrix(n, mines);
-     * 
-     * validate(n, mines, arr);
-     * 
-     * }
-     */
 }
